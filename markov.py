af86d9723e254a2beb3d2e3219184a2f31ed56be
coffeebenzene <ericteo_no1@hotmail.com>
2017-11-18 19:11:12 +0800

    partial training (part2-1)

--- markov.py
+++ markov.py
@@ -1,6 +1,26 @@
 import argparse
 
+all_y = ("O", "B-positive", "B-neutral", "B-negative", "I-positive", "I-neutral", "I-negative")
+unk_threshold = 3 # k. If word appears less than this frequency, then word is treated as unknown.
+
 def train(training_file):
+    counts_e = {}
+    counts_y = {y:0 for y in all_y}
+    for line in training_file:
+        if not line:
+            continue
+        x, y = line.split(" ") # word, tag
+        if x not in counts_e:
+            counts_e[x] = {y:0 for y in all_y}
+        counts_e[x][y] += 1
+        counts_y[y] += 1
+    
+    to_unk = []
+    counts_unk = {y:0 for y in all_y}
+    for x, y_to_x in counts_e.items():
+        if sum(y_to_x.values()) < unk_threshold:
+            to_unk.append(x)
+        #addup to counts_unk
     return None, None
     # should return emission parameters and transition parameters.
 
