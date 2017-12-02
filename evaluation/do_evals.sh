f4bb6fbd9ac99e94bb9022c217a68ac77a0833c7
coffeebenzene <ericteo_no1@hotmail.com>
2017-12-02 23:03:57 +0800

    Add prediction and evaluation of results

---
+++ evaluation/do_evals.sh
@@ -0,0 +1,12 @@
+resultfile="raw eval result.txt"
+echo -n "" > "$resultfile";
+
+for i in 2 3 4;
+do
+    echo -e "====Part $i --------------------\r" >> "$resultfile"; # \r for windows!
+    for lang in "EN" "FR" "SG" "CN";
+    do
+        echo -e "\r\n----$lang :\r" >> "$resultfile";
+        py EvalScript/evalResult.py $lang/dev.out $lang/dev.p$i.out >> "$resultfile";
+    done;
+done;
